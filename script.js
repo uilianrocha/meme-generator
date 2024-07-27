45170929e4395de8bdeb8b8df264a970c7a82bad
uilianrocha <102986858+uilianrocha@users.noreply.github.com>
2024-07-27 08:19:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+function clearAll() {
+  const memeContainer = document.querySelector(".meme-content");
+  const jokeContainer = document.querySelector(".joke-content");
+  const quoteContainer = document.querySelector(".quote-content");
+  const riddleContainer = document.querySelector(".riddle-content");
 
+  memeContainer.innerHTML = "";
+  jokeContainer.innerHTML = "";
+  quoteContainer.innerHTML = "";
+  riddleContainer.innerHTML = "";
+}
 
